829528704b2826d1e0aa8f3c376683bcdd2551a9
Raunak Kedia <raunakkedia1@gmail.com>
2020-08-26 00:56:35 +0530

    Updates

---
+++ precache-manifest.6a0f86c5cbbf23aa64634234dd430bd9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b07854db839ad145ddca68ff087bdd47",
+    "url": "/mobile-store/index.html"
+  },
+  {
+    "revision": "635620db45081aad84b3",
+    "url": "/mobile-store/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ff0f93e2c9946fe54741",
+    "url": "/mobile-store/static/css/main.496a8104.chunk.css"
+  },
+  {
+    "revision": "635620db45081aad84b3",
+    "url": "/mobile-store/static/js/2.c938b92d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mobile-store/static/js/2.c938b92d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff0f93e2c9946fe54741",
+    "url": "/mobile-store/static/js/main.e9691714.chunk.js"
+  },
+  {
+    "revision": "f644084829caba909591",
+    "url": "/mobile-store/static/js/runtime-main.f15cfd75.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/mobile-store/static/media/logo.9f7e506d.svg"
+  }
+]);
